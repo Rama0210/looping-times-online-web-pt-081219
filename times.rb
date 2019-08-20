80e98ebfce618937d256fb4c54d63c91cb1bf255
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-20 02:20:24 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -2,13 +2,4 @@
   puts "I am doing the dishes left by my former friends."
 end
  
-# > "I am doing the dishes left by my former friends."
-# > "I am doing the dishes left by my former friends."
-# > "I am doing the dishes left by my former friends."
-# > "I am doing the dishes left by my former friends."
-# > "I am doing the dishes left by my former friends."
-# > "I am doing the dishes left by my former friends."
-# > "I am doing the dishes left by my former friends."
- 
-# => 7 (return value)
 
