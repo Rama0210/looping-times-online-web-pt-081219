4c13152f4157d7c5aefbef8308c1e375e3753fe0
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-20 02:29:32 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -2,4 +2,9 @@
   puts "I am doing the dishes left by my former friends."
 end
  
-
+jewels_in_bag = 100
+ 
+3.times do 
+  puts "Hiding 10 stolen jewels."
+  jewels_in_bag = jewels_in_bag - 10
+end
