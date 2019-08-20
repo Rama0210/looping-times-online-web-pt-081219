6738dfa0820d365408a93146bfd3cedf476eb10d
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-20 02:19:50 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,14 @@
-def using_times
-	#your code here
+7.times do 
+  puts "I am doing the dishes left by my former friends."
 end
-
-
-
+ 
+# > "I am doing the dishes left by my former friends."
+# > "I am doing the dishes left by my former friends."
+# > "I am doing the dishes left by my former friends."
+# > "I am doing the dishes left by my former friends."
+# > "I am doing the dishes left by my former friends."
+# > "I am doing the dishes left by my former friends."
+# > "I am doing the dishes left by my former friends."
+ 
+# => 7 (return value)
 
